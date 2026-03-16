a2da5e91e4d71a53a4e736c5740c83888e31dbda
jeevanparmar <jeevanparmar545@gmail.com>
2026-03-16 23:40:13 +0530

    COrs change

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,9 @@ const cookieParser=require("cookie-parser");
 const cors = require('cors');
 
 app.use(cors({
-  origin: [ 'http://localhost:5173',
-    'https://vidyavanni-major-project-1.onrender.com'
+  origin: [ 
+    'https://vidyavanni-major-project-1.onrender.com',
+    'http://localhost:5173'
    ],
   credentials: true, 
 }));
